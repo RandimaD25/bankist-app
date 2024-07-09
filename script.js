e892279d6b70839dbcc73e250a28c29ebb131c0c
Randima Dias <randimamethminid525@gmail.com>
2024-07-09 18:32:11 +0530

    feat: implement sorting movements function

--- script.js
+++ script.js
@@ -57,9 +57,12 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = '';
-  movements.forEach(function (mov, i) {
+
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
@@ -216,6 +219,13 @@ btnClose.addEventListener('click', function (e) {
   inputLoginPin.blur();
 });
 
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
+});
+
 // console.log(accounts);
 
 //LECTURES
@@ -265,3 +275,48 @@ btnClose.addEventListener('click', function (e) {
 
 // const someMov = movements.some(mov => mov > 0);
 // console.log(someMov);
+
+// console.log(account4.movements.every(mov => mov > 0));
+
+// const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+// console.log(arr.flat());
+
+// const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+// console.log(arrDeep.flat(2));
+
+// const accMovements = accounts.map(acc => acc.movements);
+// console.log(accMovements);
+// const allMovements = accMovements.flat();
+// console.log(allMovements);
+
+// const allBalanace = allMovements.reduce((acc, mov) => acc + mov, 0);
+// console.log(allBalanace);
+
+// const wholeBal = accounts
+//   .flatMap(acc => acc.movements)
+//   .reduce((acc, mov) => acc + mov, 0);
+// console.log(wholeBal);
+
+const owners = ['Randima', 'Sarith', 'Surith', 'Aaman', 'Zach'];
+console.log(owners.sort());
+
+// console.log(movements.sort());
+
+// [200, 450, -400, 3000, -650, -130, 70, 1300]
+
+//ascending
+//return > 0 => B, A (switch order)
+//return < 0 => A, B (keep order)
+movements.sort((a, b) => {
+  if (a > b) return 1;
+  if (b > a) return -1;
+});
+console.log(movements);
+
+//descending
+movements.sort((a, b) => {
+  if (a > b) return -1;
+  if (b > a) return 1;
+});
+
+console.log(movements);
